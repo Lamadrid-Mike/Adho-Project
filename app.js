faf07b3b7ffa6a22652c6709896ecc1b21ac4d07
Lamadrid-Mike <mikealamadrid@hotmail.com>
2022-12-07 22:11:23 -0500

    Mobile menu has been animated

--- app.js
+++ app.js
@@ -21,9 +21,11 @@ mobileBtn.addEventListener("click", function () {
   if (document.querySelector(".links").classList.contains("hidden")) {
     document.querySelector(".links").classList.remove("hidden");
     document.querySelector(".links").classList.add("mobileMenuClass");
+    document.querySelector(".nav-bar").classList.add("change");
   } else {
     document.querySelector(".links").classList.add("hidden");
     document.querySelector(".links").classList.remove("mobileMenuClass");
+    document.querySelector(".nav-bar").classList.remove("change");
   }
 });
 
